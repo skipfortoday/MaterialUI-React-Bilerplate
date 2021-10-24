1d5afca9a7568326646f468a754df90327cf2138
rrahmadini <72929908+rrahmadini@users.noreply.github.com>
2021-10-24 17:21:49 +0700

    Update index.js
    register service workers and remove unecessary comment

--- src/index.js
+++ src/index.js
@@ -6,14 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.Fragment>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </React.Fragment>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.reregister();
